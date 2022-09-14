e6282133ec647f3b143cbbd6ac13ebdd36021525
onedayherenow <onedayherenow@gmail.com>
2022-09-14 10:56:46 -0400

    js page functionality ABI interact with EVM

---
+++ index.js
@@ -0,0 +1,25 @@
+
+// 1. Declare global variable to store the web3 instance
+
+let PetContract;
+
+// 2. Set contract address and ABI
+const Pet_Contract_Address = "";
+const Pet_Contract_ABI = [];
+
+/* 3. Prompt user to sign in to MetaMask */
+const provider = new ethers.providers.Web3Provider(window.ethereum, "goerli")
+    provider.send("eth_requestAccount", []).then(() => {
+    provider.listAccounts().then((accounts) => {
+        const signer = provider.getSigner(accounts[0]);
+
+
+    /* 3.1 Create Instance of Pet Smart Contract */
+    PetContract = new ether.Contract(
+        Pet_Contract_Address,
+        Pet_Contract_ABI,
+        signer
+        ); 
+           
+    });
+});
