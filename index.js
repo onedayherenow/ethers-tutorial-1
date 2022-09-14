08deb4e29869d4aca6f91c41cb2415d6aaf1c3c2
onedayherenow <onedayherenow@gmail.com>
2022-09-14 13:44:45 -0400

    Continueing js file/controller file

--- index.js
+++ index.js
@@ -20,6 +20,36 @@ const provider = new ethers.providers.Web3Provider(window.ethereum, "goerli")
         Pet_Contract_ABI,
         signer
         ); 
-           
+
     });
 });
+
+// 4.  Creating variables for reusable dom elements
+const petFormSection = document.querySelector(".pet-form-section");
+const showPetFormBtn = document.querySelector(".show-pet-form-btn");
+const petSection = document.querySelector(".pet-detail-section");
+const setPetButton = document.querySelector("#set-new-pet");
+const refreshBtn = document.querySelector(".refresh-pet-details-btn");
+
+
+/* 5. Function to set pet details */
+const setNewPet = () => {
+    // update button value
+    setPetButton.value = "Setting Pet...";
+}
+
+/* 5.1 Get inputs from pet form */
+const petNameInput = document.querySelector("#pet-name");
+const petOwnerInput = document.querySelector("#pet-owner");
+const petAgeInput = document.querySelector("#pet-age");
+
+/* 5.2 Getting values from the inputs */
+
+
+
+
+/* *** IN THE ABOVE CODE:
+1. WE'RE MAKING THE PetContract VARIABLE GLOBAL BECAUSE WE'LL REUSE IT IN OTHER FUNCTIONS
+
+2. PROVIDE YOUR SMART CONTRACT ADDRESS AND ABI IN THE Pet_Contract_Address AND Pet_Contract_ABI
+*/
