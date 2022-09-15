b6fbba6215636b7fab21a7d969ab29ab42947bce
onedayherenow <onedayherenow@gmail.com>
2022-09-15 14:53:07 -0400

    index.js continuing

--- index.js
+++ index.js
@@ -9,15 +9,15 @@
 let PetContract;
 
 // 2. Set contract address and ABI
-const Pet_Contract_Address = "";
+const Pet_Contract_Address = "0xB273aA4648387C318225bC68c2A48d29D36e6d63";
 const Pet_Contract_ABI = [];
 
 /* 3. Prompt user to sign in to MetaMask */
-const provider = new ethers.providers.Web3Provider(window.ethereum, "goerli")
-    provider.send("eth_requestAccount", []).then(() => {
-    provider.listAccounts().then((accounts) => {
-        const signer = provider.getSigner(accounts[0]);
-
+const provider = new ethers.providers.Web3Provider(window.ethereum, "goerli")    // this is also known as the 'node' provider
+    
+provider.send("eth_requestAccount", []).then( () => {
+    provider.listAccounts().then((accounts) => {    // creates an array called accounts with the first index [0] filled with first account address
+        const signer = provider.getSigner(accounts[0]);  // assigns first account as the signer
 
     /* 3.1 Create Instance of Pet Smart Contract */
     PetContract = new ether.Contract(
@@ -92,6 +92,34 @@ PetContract.setPet(petName, petOwner, petAge)
 
 /* Function to set pet details on click of button */
 setPetButton.addEventListener("click", setNewPet);
+
+
+
+/* 6. Function to get pet details */
+const getCurrentPet = async () => {   
+    setPetButton.value = "Getting Pet...";
+
+    /* 6.1 Get pet details from smart contract */
+    const pet = await PetContract.getPet();
+
+
+    /* 6.2 Display the pet details section */
+
+        // 6.2.1 Hide the pet form in DOM 
+        petSection.getElementsByClassName.display = "block";
+        petFormSection.getElementsByClassName.display = "none";
+
+    /* 6.3 Pet is an array of 3 strings [petName, petOwner, petAge]  */
+    const petName = pet[0];
+    const petOwner = pet[1];
+    const petAge = pet[2];
+
+    /* 6.4 Display pet details in DOM */
+    document.querySelector(".pet-detail-name").innerText = petName;
+    document.querySelector(".pet-detail-owner").innetText = petOwner;
+    document.querySelector(".pet-detail-age").innerText = petAge;
+
+};
 
 
 /* *** IN THE ABOVE CODE:
